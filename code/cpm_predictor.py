7ded35895147757d6bb1f83e554d397e5b6811c8
shivakumarJ <electrodude1995@gmail.com>
2020-10-08 00:25:59 +0530

    Add functionality for used to select train and test csv

--- code/cpm_predictor.py
+++ code/cpm_predictor.py
@@ -8,17 +8,18 @@ import ast
 import datetime as dt
 import seaborn as sns
 import pickle
+import tkinter as tk
 from pathlib import Path
 from catboost import CatBoostRegressor
 from sklearn.preprocessing import OrdinalEncoder
 from sklearn.metrics import mean_absolute_error
+from tkinter import filedialog
 
-def read_dataset():
+def read_dataset(train_path, test_path):
     # Read the train dataset as well. no need to take as user input.
-    train = pd.read_csv(r"C:\Users\blahb\OneDrive\Documents\ML and Cloud UPGRAD\CAPSTONE PROJECT\CPM-prediction-with-MiQ\data\train.csv")
-    
+    train = pd.read_csv(train_path)
     # Read the path of the test dataset
-    test = pd.read_csv(r"C:\Users\blahb\OneDrive\Documents\ML and Cloud UPGRAD\CAPSTONE PROJECT\CPM-prediction-with-MiQ\data\test.csv")
+    test = pd.read_csv(test_path)
 
     # convert dtype of date
     # Infer the date from the input.
@@ -26,6 +27,7 @@ def read_dataset():
     target_date = str(test['Date'].iloc[0].date())
 
     train['Date'] = pd.to_datetime(train['Date'], format = '%d-%m-%Y')
+    validation_date = str(pd.Series(train['Date'].sort_values().unique()).iloc[-1].date())
     
     # print some information such as shape of input dataframe, 
     print("Shape of train data: {}".format(train.shape))
@@ -33,7 +35,7 @@ def read_dataset():
     
     # and the inferred target date 
     print("Target date: {}".format(target_date))
-    return train, test, target_date
+    return train, test, target_date, validation_date
 
 
 def clean_unknowns(train, test):
@@ -47,6 +49,10 @@ def clean_unknowns(train, test):
     # train set
     unknown_app_ids = train[train['App/URL ID'] == 'Unknown'].index
     train.drop(unknown_app_ids, inplace = True)
+
+    # Reset index
+    test.reset_index(drop = True, inplace = True)
+    train.reset_index(drop = True, inplace = True)
 
     # Perform inplace transformation, no need to return dataframe
     # print the shape of train and test after removing "Unknown".
@@ -77,11 +83,8 @@ def prep_dataset(train, test):
     # return final_df
     return final_df
 
-def ordinal_encoding(df):
-    categorical = ['app_url_id', 'isp_or_carrier_id', 'device_type',\
-        'exchange_id','operating_system', 'browser', \
-            'creative_size', 'advertiser_currency','day_of_week']
-
+def ordinal_encoding(df, categorical):
+    
     enc = OrdinalEncoder()
     # encode the categorical columns of the final_df
     df[categorical] = enc.fit_transform(df[categorical])
@@ -90,13 +93,15 @@ def ordinal_encoding(df):
     return df
 
 
-def train_test_split(df, target_date):
+def train_test_split(df, target_date, validation_date, categorical):
     # create df_test from the target_date
-    print(target_date)
+    print("Test Date: ", target_date)
+    print("Validation Date: ", validation_date)
+
     df_test = df[df['date'] == target_date].copy()
   
     # create validation dataset frm train data.
-    df_val = df[df['date'] == '2020-08-27'].copy()
+    df_val = df[df['date'] == validation_date].copy()
     
     # drop "date" and "cpm".
     df_test.drop(['cpm', 'date'], inplace = True, axis = 1)
@@ -104,13 +109,17 @@ def train_test_split(df, target_date):
 
     print("Shape of validation set: {}".format(df_val.shape))
     print("Shape of test set: {}".format(df_test.shape))
+
+    # Convert values to integer
+    df_test[categorical] = df_test[categorical].apply(pd.to_numeric, downcast = 'integer')
+    df_val[categorical] = df_val[categorical].apply(pd.to_numeric, downcast = 'integer')
     
     # return the df_test, df_validation.
     return df_test, df_val
 
 def model_prediction(df):
     # load the model (catboostmodel.pkl)
-    cbr = pickle.load(open(r'C:\Users\blahb\OneDrive\Documents\ML and Cloud UPGRAD\CAPSTONE PROJECT\CPM-prediction-with-MiQ\code\output\catboostmodel.pkl', 'rb'))
+    cbr = pickle.load(open(r'code\output\catboostmodel_1.pkl', 'rb'))
 
     # print the MAE for train and test
     # print R2 for  train and test.
@@ -120,14 +129,20 @@ def model_prediction(df):
     # merge the output series with the original test dataset.
     test['predicted_cpm'] = predicted_cpm
 
-    # write the output dataset to csv and return path to output.
-    test.to_csv(r"../data/prediction.csv")
-    print("Predictions file at output path")
+    return test
 
 if __name__ == '__main__':
     # take user input to the path of the test dataset.
-    #test_path = input()
-    train, test, date = read_dataset()
+    root = tk.Tk()
+    root.withdraw()
+    
+    # Select the train and test data from their respective path
+    print("Select test.csv file")
+    test_path = filedialog.askopenfilename()
+    print("Select train.csv file")
+    train_path= filedialog.askopenfilename()
+
+    train, test, target_date, validation_date = read_dataset(train_path, test_path)
     
     # clean the "unknowns" present in train and test set
     clean_unknowns(train, test)
@@ -136,16 +151,23 @@ if __name__ == '__main__':
     # rename columns, fillna  etc
     final_df = prep_dataset(train, test)
 
+    # Categorical Columns 
+    categorical = ['app_url_id', 'isp_or_carrier_id', 'device_type',\
+        'exchange_id','operating_system', 'browser', \
+            'creative_size', 'advertiser_currency','day_of_week']
+
     # ordinal encoding the data
-    final_df = ordinal_encoding(final_df)
-
+    final_df = ordinal_encoding(final_df, categorical)
+    
     # dataset splitting
-    df_test, df_val = train_test_split(final_df, date)
+    df_test, df_val = train_test_split(final_df, target_date, validation_date, categorical)
 
-    # model prediction from saved location
-    print("Shape of df_test: ",df_test.head())
+    result = model_prediction(df_test)
+    
+    # write the output dataset to csv and return path to output.
+    result.to_csv(r"code\output\prediction_2.csv", index = False)
+    print("Predictions file at output path: {}".format(r"code\output\prediction_2.csv"))
 
-    #model_prediction(df_test)
 
 
 
